6cbb29305cfb8f79e7eac08d01602c26db63757e
hogimn <hogimn@gmail.com>
2021-03-24 09:07:15 -0700

    first release for rpi-3b

---
+++ install.sh
@@ -0,0 +1,4 @@
+mkdir -p bin
+git clone https://github.com/raspberrypi/linux -b rpi-4.19.y --depth=1
+git clone https://github.com/raspberrypi/tools bin/tools
+git clone https://github.com/hogimn/build -b rpi-3b
